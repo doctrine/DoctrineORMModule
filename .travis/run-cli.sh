7f3ece618d6869f3a8b559e36983455468a2c840
Marco Pivetta <ocramius@gmail.com>
2012-04-16 01:33:30 +0200

    Fixing CLI paths(was using submodule paths instead of composer

--- .travis/run-cli.sh
+++ .travis/run-cli.sh
@@ -1,16 +1,16 @@
-./vendor/DoctrineModule/bin/doctrine dbal:import ./vendor/DoctrineORMModule/.travis/dummy-import.sql
-./vendor/DoctrineModule/bin/doctrine dbal:run-sql "SELECT 1"
-./vendor/DoctrineModule/bin/doctrine orm:clear-cache:metadata
-./vendor/DoctrineModule/bin/doctrine orm:clear-cache:query
-./vendor/DoctrineModule/bin/doctrine orm:clear-cache:result
-./vendor/DoctrineModule/bin/doctrine orm:clear-cache:query
-./vendor/DoctrineModule/bin/doctrine orm:ensure-production-settings
-./vendor/DoctrineModule/bin/doctrine orm:generate-proxies
-./vendor/DoctrineModule/bin/doctrine orm:info
-./vendor/DoctrineModule/bin/doctrine orm:run-dql "SELECT COUNT(a) FROM TravisTest\TestEntity a"
-./vendor/DoctrineModule/bin/doctrine orm:schema-tool:create
-./vendor/DoctrineModule/bin/doctrine orm:schema-tool:update
-./vendor/DoctrineModule/bin/doctrine orm:schema-tool:validate-schema
-./vendor/DoctrineModule/bin/doctrine orm:schema-tool:drop
-./vendor/DoctrineModule/bin/doctrine migrations:generate --configuration=vendor/DoctrineORMModule/.travis/migrations-config.xml
-./vendor/DoctrineModule/bin/doctrine migrations:diff --configuration=vendor/DoctrineORMModule/.travis/migrations-config.xml
+./vendor/doctrine/DoctrineModule/bin/doctrine dbal:import ./vendor/doctrine/DoctrineORMModule/.travis/dummy-import.sql
+./vendor/doctrine/DoctrineModule/bin/doctrine dbal:run-sql "SELECT 1"
+./vendor/doctrine/DoctrineModule/bin/doctrine orm:clear-cache:metadata
+./vendor/doctrine/DoctrineModule/bin/doctrine orm:clear-cache:query
+./vendor/doctrine/DoctrineModule/bin/doctrine orm:clear-cache:result
+./vendor/doctrine/DoctrineModule/bin/doctrine orm:clear-cache:query
+./vendor/doctrine/DoctrineModule/bin/doctrine orm:ensure-production-settings
+./vendor/doctrine/DoctrineModule/bin/doctrine orm:generate-proxies
+./vendor/doctrine/DoctrineModule/bin/doctrine orm:info
+./vendor/doctrine/DoctrineModule/bin/doctrine orm:run-dql "SELECT COUNT(a) FROM TravisTest\TestEntity a"
+./vendor/doctrine/DoctrineModule/bin/doctrine orm:schema-tool:create
+./vendor/doctrine/DoctrineModule/bin/doctrine orm:schema-tool:update
+./vendor/doctrine/DoctrineModule/bin/doctrine orm:schema-tool:validate-schema
+./vendor/doctrine/DoctrineModule/bin/doctrine orm:schema-tool:drop
+./vendor/doctrine/DoctrineModule/bin/doctrine migrations:generate --configuration=vendor/doctrine/DoctrineORMModule/.travis/migrations-config.xml
+./vendor/doctrine/DoctrineModule/bin/doctrine migrations:diff --configuration=vendor/doctrine/DoctrineORMModule/.travis/migrations-config.xml
