d5f46f0ba886375292c4ce0ee65b04ecb3242213
Marco Pivetta <ocramius@gmail.com>
2012-04-16 03:30:33 +0200

    Adding travis test configuration for CLI

--- .travis/run-cli.sh
+++ .travis/run-cli.sh
@@ -4,13 +4,13 @@
 ./vendor/doctrine/DoctrineModule/bin/doctrine orm:clear-cache:query
 ./vendor/doctrine/DoctrineModule/bin/doctrine orm:clear-cache:result
 ./vendor/doctrine/DoctrineModule/bin/doctrine orm:clear-cache:query
-./vendor/doctrine/DoctrineModule/bin/doctrine orm:ensure-production-settings
 ./vendor/doctrine/DoctrineModule/bin/doctrine orm:generate-proxies
+./vendor/doctrine/DoctrineModule/bin/doctrine orm:ensure-production-settings
 ./vendor/doctrine/DoctrineModule/bin/doctrine orm:info
-./vendor/doctrine/DoctrineModule/bin/doctrine orm:run-dql "SELECT COUNT(a) FROM TravisTest\TestEntity a"
 ./vendor/doctrine/DoctrineModule/bin/doctrine orm:schema-tool:create
 ./vendor/doctrine/DoctrineModule/bin/doctrine orm:schema-tool:update
-./vendor/doctrine/DoctrineModule/bin/doctrine orm:schema-tool:validate-schema
+./vendor/doctrine/DoctrineModule/bin/doctrine orm:validate-schema
+./vendor/doctrine/DoctrineModule/bin/doctrine orm:run-dql "SELECT COUNT(a) FROM DoctrineORMModuleTest\Assets\Entity\Test a"
 ./vendor/doctrine/DoctrineModule/bin/doctrine orm:schema-tool:drop
 ./vendor/doctrine/DoctrineModule/bin/doctrine migrations:generate --configuration=vendor/doctrine/DoctrineORMModule/.travis/migrations-config.xml
 ./vendor/doctrine/DoctrineModule/bin/doctrine migrations:diff --configuration=vendor/doctrine/DoctrineORMModule/.travis/migrations-config.xml
