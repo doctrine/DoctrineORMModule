2678d88d5c607afa1efc101c4c3cdc73b1b5a55e
Marco Pivetta <ocramius@gmail.com>
2012-05-11 23:14:32 +0200

    Stripping classmap in favour of standard autoloader during development

--- autoload_register.php
+++ autoload_register.php
@@ -1,2 +1,6 @@
 <?php
 spl_autoload_register(include __DIR__ . '/autoload_function.php');
+
+require_once(__DIR__ . '/Module.php');
+$module = new DoctrineORMModule\Module();
+Zend\Loader\AutoloaderFactory::factory($module->getAutoloaderConfig());
