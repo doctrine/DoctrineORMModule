ee07234900d5cfa93598829fdb58373479480ed5
Marco Pivetta <ocramius@gmail.com>
2012-05-13 23:30:01 +0200

    Fixing doctrine CLI path as of latest changes in DoctrineModule

--- .travis/run-cli.sh
+++ .travis/run-cli.sh
@@ -1,16 +1,16 @@
-./vendor/bin/doctrine dbal:import ./vendor/doctrine/DoctrineORMModule/.travis/dummy-import.sql
-./vendor/bin/doctrine dbal:run-sql "SELECT 1"
-./vendor/bin/doctrine orm:clear-cache:metadata
-./vendor/bin/doctrine orm:clear-cache:query
-./vendor/bin/doctrine orm:clear-cache:result
-./vendor/bin/doctrine orm:clear-cache:query
-./vendor/bin/doctrine orm:generate-proxies
-./vendor/bin/doctrine orm:ensure-production-settings
-./vendor/bin/doctrine orm:info
-./vendor/bin/doctrine orm:schema-tool:create
-./vendor/bin/doctrine orm:schema-tool:update
-./vendor/bin/doctrine orm:validate-schema
-./vendor/bin/doctrine orm:run-dql "SELECT COUNT(a) FROM DoctrineORMModuleTest\Assets\Entity\Test a"
-./vendor/bin/doctrine orm:schema-tool:drop
-./vendor/bin/doctrine migrations:generate --configuration=vendor/doctrine/DoctrineORMModule/.travis/migrations-config.xml
-./vendor/bin/doctrine migrations:diff --configuration=vendor/doctrine/DoctrineORMModule/.travis/migrations-config.xml
+./vendor/bin/doctrine-module dbal:import ./vendor/doctrine/DoctrineORMModule/.travis/dummy-import.sql
+./vendor/bin/doctrine-module dbal:run-sql "SELECT 1"
+./vendor/bin/doctrine-module orm:clear-cache:metadata
+./vendor/bin/doctrine-module orm:clear-cache:query
+./vendor/bin/doctrine-module orm:clear-cache:result
+./vendor/bin/doctrine-module orm:clear-cache:query
+./vendor/bin/doctrine-module orm:generate-proxies
+./vendor/bin/doctrine-module orm:ensure-production-settings
+./vendor/bin/doctrine-module orm:info
+./vendor/bin/doctrine-module orm:schema-tool:create
+./vendor/bin/doctrine-module orm:schema-tool:update
+./vendor/bin/doctrine-module orm:validate-schema
+./vendor/bin/doctrine-module orm:run-dql "SELECT COUNT(a) FROM DoctrineORMModuleTest\Assets\Entity\Test a"
+./vendor/bin/doctrine-module orm:schema-tool:drop
+./vendor/bin/doctrine-module migrations:generate --configuration=vendor/doctrine/DoctrineORMModule/.travis/migrations-config.xml
+./vendor/bin/doctrine-module migrations:diff --configuration=vendor/doctrine/DoctrineORMModule/.travis/migrations-config.xml
