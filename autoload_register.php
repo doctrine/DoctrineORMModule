0192d873f2ddad8d4a2528f0270860000fc29daf
Kyle Spraggs <theman@spiffyjr.me>
2012-05-22 22:57:03 -0500

    Remove unused autoload files. Updated composer to track dev-feature/beta4.

--- autoload_register.php
+++
@@ -1,6 +0,0 @@
-<?php
-spl_autoload_register(include __DIR__ . '/autoload_function.php');
-
-require_once(__DIR__ . '/Module.php');
-$module = new DoctrineORMModule\Module();
-Zend\Loader\AutoloaderFactory::factory($module->getAutoloaderConfig());
