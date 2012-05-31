45e2b248028c7c6ba2bb0b826dbf507e4e5caf37
Michaël Gallego <mic.gallego@gmail.com>
2012-05-31 23:36:52 +0200

    Allow to set fetchJoin for paginator

--- src/DoctrineORMModule/Paginator/Adapter.php
+++ src/DoctrineORMModule/Paginator/Adapter.php
@@ -1,82 +1,91 @@
 <?php
-    /*
-    * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-    * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-    * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-    * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-    * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-    * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-    * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-    * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-    * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-    * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-    * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-    *
-    * This software consists of voluntary contributions made by many individuals
-    * and is licensed under the MIT license. For more information, see
-    * <http://www.doctrine-project.org>.
-    */
+/*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+* A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+* OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+* SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+* LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+* DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+* THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+* This software consists of voluntary contributions made by many individuals
+* and is licensed under the MIT license. For more information, see
+* <http://www.doctrine-project.org>.
+*/
 
-    namespace DoctrineORMModule\Paginator;
+namespace DoctrineORMModule\Paginator;
 
-    use Doctrine\ORM\Tools\Pagination\Paginator;
-    use Doctrine\ORM\QueryBuilder;
-    use Zend\Paginator\Adapter\AdapterInterface;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+use Doctrine\ORM\QueryBuilder;
+use Zend\Paginator\Adapter\AdapterInterface;
+
+/**
+ * Paginator adapter for Zend Paginator
+ *
+ * @license MIT
+ * @link    http://www.doctrine-project.org/
+ * @since   0.1.0
+ * @author  Tõnis Tobre <tobre@bitweb.ee>
+ */
+class Adapter implements AdapterInterface
+{
+    /**
+     * @var Paginator
+     */
+    protected $paginator;
 
     /**
-     * Paginator adapter for Zend Paginator
+     * Constructor
      *
-     * @license MIT
-     * @link    http://www.doctrine-project.org/
-     * @since   0.1.0
-     * @author  Tõnis Tobre <tobre@bitweb.ee>
+     * @param Paginator $paginator
      */
-    class Adapter implements AdapterInterface {
-
-        /**
-         * @var Paginator
-         */
-        protected $paginator;
-
-        /**
-         * Constructor
-         *
-         * @param Paginator $paginator
-         */
-        public function __construct(Paginator $paginator = null) {
-            if (!is_null($paginator)) {
-                $this->setPaginator($paginator);
-            }
-        }
-
-        /**
-         * @param Paginator $paginator
-         */
-        public function setPaginator(Paginator $paginator) {
-            $this->paginator = $paginator;
-        }
-
-        /**
-         * @return \Doctrine\ORM\Tools\Pagination\Paginator
-         */
-        public function getPaginator() {
-            return $this->paginator;
-        }
-
-        /**
-         * {@inheritDoc}
-         */
-        public function getItems($offset, $itemCountPerPage) {
-            $this->paginator->getQuery()->setFirstResult($offset)
-                ->setMaxResults($itemCountPerPage);
-
-            return $this->paginator->getIterator();
-        }
-
-        /**
-         * {@inheritDoc}
-         */
-        public function count() {
-            return $this->paginator->count();
-        }
+    public function __construct(Paginator $paginator)
+    {
+        $this->paginator = $paginator;
     }
+
+    /**
+     * @param Paginator $paginator
+     * @return Adapter
+     */
+    public function setPaginator(Paginator $paginator)
+    {
+        $this->paginator = $paginator;
+        return $this;
+    }
+
+    /**
+     * @return \Doctrine\ORM\Tools\Pagination\Paginator
+     */
+    public function getPaginator()
+    {
+        return $this->paginator;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getItems($offset, $itemCountPerPage)
+    {
+        $this->paginator
+            ->getQuery()
+            ->setFirstResult($offset)
+            ->setMaxResults($itemCountPerPage);
+
+        return $this->paginator
+                    ->getIterator();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function count()
+    {
+        return $this->paginator
+                    ->count();
+    }
+}
