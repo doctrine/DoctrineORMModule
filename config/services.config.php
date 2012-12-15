afe01ec29be76f5ff2633bd9806aa8e90f6aa543
Marco Pivetta <ocramius@gmail.com>
2012-12-15 06:46:27 +0100

    Moving CLI tests to PHPUnit, typos

---
+++ config/services.config.php
@@ -0,0 +1,95 @@
+<?php
+return array(
+    'doctrine' => array(
+        'orm_autoload_annotations' => true,
+
+        'connection' => array(
+            'orm_default' => array(
+                'configuration' => 'orm_default',
+                'eventmanager'  => 'orm_default',
+
+                'params' => array(
+                    'host'     => 'localhost',
+                    'port'     => '3306',
+                    'user'     => 'username',
+                    'password' => 'password',
+                    'dbname'   => 'database',
+                )
+            ),
+        ),
+
+        'configuration' => array(
+            'orm_default' => array(
+                'metadata_cache'    => 'array',
+                'query_cache'       => 'array',
+                'result_cache'      => 'array',
+
+                'driver'            => 'orm_default',
+
+                'generate_proxies'  => true,
+                'proxy_dir'         => 'data/DoctrineORMModule/Proxy',
+                'proxy_namespace'   => 'DoctrineORMModule\Proxy',
+                'filters'           => array()
+            )
+        ),
+
+        'driver' => array(
+            'orm_default' => array(
+                'class'   => 'Doctrine\ORM\Mapping\Driver\DriverChain',
+                'drivers' => array()
+            )
+        ),
+
+        'entitymanager' => array(
+            'orm_default' => array(
+                'connection'    => 'orm_default',
+                'configuration' => 'orm_default'
+            )
+        ),
+
+        'eventmanager' => array(
+            'orm_default' => array()
+        ),
+
+        'sql_logger_collector' => array(
+            'orm_default' => array(),
+        ),
+
+        'entity_resolver' => array(
+            'orm_default' => array()
+        ),
+
+        'authentication' => array(
+            'orm_default' => array(
+                'objectManager' => 'doctrine.entitymanager.orm_default',
+                //'identityClass' => 'Application\Model\User',
+                //'identityProperty' => 'username',
+                //'credentialProperty' => 'password'
+            ),
+        ),
+    ),
+
+    // zendframework/zend-developer-tools specific settings
+
+    'view_manager' => array(
+        'template_map' => array(
+            'zend-developer-tools/toolbar/doctrine-orm-queries'  => __DIR__ . '/../view/zend-developer-tools/toolbar/doctrine-orm-queries.phtml',
+            'zend-developer-tools/toolbar/doctrine-orm-mappings' => __DIR__ . '/../view/zend-developer-tools/toolbar/doctrine-orm-mappings.phtml',
+        ),
+    ),
+
+    'zenddevelopertools' => array(
+        'profiler' => array(
+            'collectors' => array(
+                'orm_default'  => 'doctrine.sql_logger_collector.orm_default',
+                'orm_default_mappings' => 'doctrine.mapping_collector.orm_default',
+            ),
+        ),
+        'toolbar' => array(
+            'entries' => array(
+                'orm_default'  => 'zend-developer-tools/toolbar/doctrine-orm-queries',
+                'orm_default_mappings' => 'zend-developer-tools/toolbar/doctrine-orm-mappings',
+            ),
+        ),
+    ),
+);
