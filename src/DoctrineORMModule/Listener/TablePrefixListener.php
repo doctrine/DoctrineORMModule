c097ae6c29c2d0de8bff7f38bab8a672bafb9998
exoon <exoon@online.de>
2013-01-02 11:56:59 +0100

    missing files

---
+++ src/DoctrineORMModule/Listener/TablePrefixListener.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+* A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+* OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+* SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+* LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+* DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+* THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+* This software consists of voluntary contributions made by many individuals
+* and is licensed under the MIT license. For more information, see
+* <http://www.doctrine-project.org>.
+*/
+
+// info: http://docs.doctrine-project.org/en/2.0.x/cookbook/sql-table-prefixes.html
+
+namespace DoctrineORMModule\Listener;
+
+use \Doctrine\ORM\Event\LoadClassMetadataEventArgs;
+
+class TablePrefixListener
+{
+	protected $prefix = '';
+
+	public function __construct($prefix)
+	{
+		$this->prefix = (string) $prefix;
+	}
+
+	public function loadClassMetadata(LoadClassMetadataEventArgs $eventArgs)
+	{
+		$classMetadata = $eventArgs->getClassMetadata();
+		$classMetadata->setTableName($this->prefix . $classMetadata->getTableName());
+		foreach ($classMetadata->getAssociationMappings() as $fieldName => $mapping) {
+			if ($mapping['type'] == \Doctrine\ORM\Mapping\ClassMetadataInfo::MANY_TO_MANY) {
+				$mappedTableName = $classMetadata->associationMappings[$fieldName]['joinTable']['name'];
+				$classMetadata->associationMappings[$fieldName]['joinTable']['name'] = $this->prefix . $mappedTableName;
+			}
+		}
+	}
+}
