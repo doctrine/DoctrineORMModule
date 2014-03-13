3697fa293bb382946931e5dda2da27a22777af2c
mpalourdio <mpalourdio@gmail.com>
2014-03-13 12:25:15 +0100

    CS fix

--- src/DoctrineORMModule/Form/Annotation/AnnotationBuilder.php
+++ src/DoctrineORMModule/Form/Annotation/AnnotationBuilder.php
@@ -72,9 +72,9 @@ class AnnotationBuilder extends ZendAnnotationBuilder
      */
     public function getFormSpecification($entity)
     {
-        $formSpec    = parent::getFormSpecification($entity);
-        $metadata    = $this->objectManager->getClassMetadata(is_object($entity) ? get_class($entity) : $entity);
-        $inputFilter = $formSpec['input_filter'];
+        $formSpec     = parent::getFormSpecification($entity);
+        $metadata     = $this->objectManager->getClassMetadata(is_object($entity) ? get_class($entity) : $entity);
+        $inputFilter  = $formSpec['input_filter'];
 
         $formElements = array(
             'DoctrineModule\Form\Element\ObjectSelect',
@@ -85,7 +85,7 @@ class AnnotationBuilder extends ZendAnnotationBuilder
         foreach ($formSpec['elements'] as $key => $elementSpec) {
             $name          = isset($elementSpec['spec']['name']) ? $elementSpec['spec']['name'] : null;
             $isFormElement = (isset($elementSpec['spec']['type']) &&
-                in_array($elementSpec['spec']['type'], $formElements));
+                              in_array($elementSpec['spec']['type'], $formElements));
 
             if (!$name) {
                 continue;
