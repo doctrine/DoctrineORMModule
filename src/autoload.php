6f675fa8da97b2506a2ea6896e9d43905cfa092e
webimpress <contact@webimpress.com>
2019-01-10 10:16:35 +0000

    Wrap long lines in autoload.php - PHPCS fix

--- src/autoload.php
+++ src/autoload.php
@@ -5,18 +5,30 @@
 // and here we are creating aliases in case v1 is used.
 
 $classMap = [
-    Doctrine\DBAL\Migrations\Configuration\Configuration::class => Doctrine\Migrations\Configuration\Configuration::class,
-    Doctrine\DBAL\Migrations\OutputWriter::class => Doctrine\Migrations\OutputWriter::class,
-    Doctrine\DBAL\Migrations\AbstractMigration::class => Doctrine\Migrations\AbstractMigration::class,
-    Doctrine\DBAL\Migrations\Tools\Console\Command\AbstractCommand::class => Doctrine\Migrations\Tools\Console\Command\AbstractCommand::class,
-    Doctrine\DBAL\Migrations\Tools\Console\Command\DiffCommand::class => Doctrine\Migrations\Tools\Console\Command\DiffCommand::class,
-    Doctrine\DBAL\Migrations\Tools\Console\Command\ExecuteCommand::class => Doctrine\Migrations\Tools\Console\Command\ExecuteCommand::class,
-    Doctrine\DBAL\Migrations\Tools\Console\Command\GenerateCommand::class => Doctrine\Migrations\Tools\Console\Command\GenerateCommand::class,
-    Doctrine\DBAL\Migrations\Tools\Console\Command\LatestCommand::class => Doctrine\Migrations\Tools\Console\Command\LatestCommand::class,
-    Doctrine\DBAL\Migrations\Tools\Console\Command\MigrateCommand::class => Doctrine\Migrations\Tools\Console\Command\MigrateCommand::class,
-    Doctrine\DBAL\Migrations\Tools\Console\Command\StatusCommand::class => Doctrine\Migrations\Tools\Console\Command\StatusCommand::class,
-    Doctrine\DBAL\Migrations\Tools\Console\Command\UpToDateCommand::class => Doctrine\Migrations\Tools\Console\Command\UpToDateCommand::class,
-    Doctrine\DBAL\Migrations\Tools\Console\Command\VersionCommand::class => Doctrine\Migrations\Tools\Console\Command\VersionCommand::class,
+    Doctrine\DBAL\Migrations\Configuration\Configuration::class
+        => Doctrine\Migrations\Configuration\Configuration::class,
+    Doctrine\DBAL\Migrations\OutputWriter::class
+        => Doctrine\Migrations\OutputWriter::class,
+    Doctrine\DBAL\Migrations\AbstractMigration::class
+        => Doctrine\Migrations\AbstractMigration::class,
+    Doctrine\DBAL\Migrations\Tools\Console\Command\AbstractCommand::class
+        => Doctrine\Migrations\Tools\Console\Command\AbstractCommand::class,
+    Doctrine\DBAL\Migrations\Tools\Console\Command\DiffCommand::class
+        => Doctrine\Migrations\Tools\Console\Command\DiffCommand::class,
+    Doctrine\DBAL\Migrations\Tools\Console\Command\ExecuteCommand::class
+        => Doctrine\Migrations\Tools\Console\Command\ExecuteCommand::class,
+    Doctrine\DBAL\Migrations\Tools\Console\Command\GenerateCommand::class
+        => Doctrine\Migrations\Tools\Console\Command\GenerateCommand::class,
+    Doctrine\DBAL\Migrations\Tools\Console\Command\LatestCommand::class
+        => Doctrine\Migrations\Tools\Console\Command\LatestCommand::class,
+    Doctrine\DBAL\Migrations\Tools\Console\Command\MigrateCommand::class
+        => Doctrine\Migrations\Tools\Console\Command\MigrateCommand::class,
+    Doctrine\DBAL\Migrations\Tools\Console\Command\StatusCommand::class
+        => Doctrine\Migrations\Tools\Console\Command\StatusCommand::class,
+    Doctrine\DBAL\Migrations\Tools\Console\Command\UpToDateCommand::class
+        => Doctrine\Migrations\Tools\Console\Command\UpToDateCommand::class,
+    Doctrine\DBAL\Migrations\Tools\Console\Command\VersionCommand::class
+        => Doctrine\Migrations\Tools\Console\Command\VersionCommand::class,
 ];
 
 foreach ($classMap as $legacy => $newClass) {
