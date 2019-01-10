639ab21752ee8107f5c72e628e20c48b1cd0419d
webimpress <contact@webimpress.com>
2019-01-10 10:15:32 +0000

    Added UpToDate command
    Bumped minimum version of doctrine/migrations to 1.5 as that command is
    available since this version.

--- src/autoload.php
+++ src/autoload.php
@@ -15,6 +15,7 @@ $classMap = [
     Doctrine\DBAL\Migrations\Tools\Console\Command\LatestCommand::class => Doctrine\Migrations\Tools\Console\Command\LatestCommand::class,
     Doctrine\DBAL\Migrations\Tools\Console\Command\MigrateCommand::class => Doctrine\Migrations\Tools\Console\Command\MigrateCommand::class,
     Doctrine\DBAL\Migrations\Tools\Console\Command\StatusCommand::class => Doctrine\Migrations\Tools\Console\Command\StatusCommand::class,
+    Doctrine\DBAL\Migrations\Tools\Console\Command\UpToDateCommand::class => Doctrine\Migrations\Tools\Console\Command\UpToDateCommand::class,
     Doctrine\DBAL\Migrations\Tools\Console\Command\VersionCommand::class => Doctrine\Migrations\Tools\Console\Command\VersionCommand::class,
 ];
 
