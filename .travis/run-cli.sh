b5a0e965e1f3f887df0f37330dbd91708c62f2d7
webimpress <contact@webimpress.com>
2019-07-02 19:18:04 +0100

    Added migrations:migrate command to cli command runner

--- .travis/run-cli.sh
+++ .travis/run-cli.sh
@@ -16,3 +16,4 @@
 ./vendor/bin/doctrine-module migrations:generate
 ./vendor/bin/doctrine-module migrations:diff
 ./vendor/bin/doctrine-module migrations:execute 20120714005702 -n
+./vendor/bin/doctrine-module migrations:migrate
