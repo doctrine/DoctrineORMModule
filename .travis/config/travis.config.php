cf487413ef69f6b97ee25b67967dbfa5d4e0dd17
Tom H Anderson <tom.h.anderson@gmail.com>
2020-03-07 01:24:59 -0700

    Use PDOMySql for testing

--- .travis/config/travis.config.php
+++ .travis/config/travis.config.php
@@ -6,6 +6,7 @@ return [
     'doctrine' => [
         'connection' => [
             'orm_default' => [
+                'driverClass' => 'Doctrine\\DBAL\\Driver\\PDOMySql\\Driver',
                 'params' => [
                     'user'     => 'root',
                     'password' => '',
