875c7d520bf7f839eb7f7cb8302fbef3c721994f
Tom H Anderson <tom.h.anderson@gmail.com>
2020-03-07 01:03:31 -0700

    Convert travis cli tests to run docker

--- .travis/run-cli.sh
+++ .travis/run-cli.sh
@@ -1,19 +1,19 @@
-./vendor/bin/doctrine-module dbal:import .travis/dummy-import.sql
-./vendor/bin/doctrine-module dbal:run-sql "SELECT 1"
-./vendor/bin/doctrine-module orm:clear-cache:metadata
-./vendor/bin/doctrine-module orm:clear-cache:query
-./vendor/bin/doctrine-module orm:clear-cache:result
-./vendor/bin/doctrine-module orm:generate-proxies
-./vendor/bin/doctrine-module orm:ensure-production-settings
-./vendor/bin/doctrine-module orm:info
-./vendor/bin/doctrine-module orm:schema-tool:create
-./vendor/bin/doctrine-module orm:schema-tool:update
-./vendor/bin/doctrine-module orm:validate-schema
-./vendor/bin/doctrine-module dbal:run-sql "SELECT COUNT(a.id) FROM entity a"
-./vendor/bin/doctrine-module orm:run-dql "SELECT COUNT(a) FROM DoctrineORMModule\Travis\Entity\Entity a"
-./vendor/bin/doctrine-module orm:schema-tool:drop --dump-sql
-./vendor/bin/doctrine-module orm:schema-tool:drop --force
-./vendor/bin/doctrine-module migrations:generate
-./vendor/bin/doctrine-module migrations:diff
-./vendor/bin/doctrine-module migrations:execute 20120714005702 -n
-./vendor/bin/doctrine-module migrations:migrate
+travis_retry docker-compose run --rm php ./vendor/bin/doctrine-module dbal:import .travis/dummy-import.sql
+travis_retry docker-compose run --rm php ./vendor/bin/doctrine-module dbal:run-sql "SELECT 1"
+travis_retry docker-compose run --rm php ./vendor/bin/doctrine-module orm:clear-cache:metadata
+travis_retry docker-compose run --rm php ./vendor/bin/doctrine-module orm:clear-cache:query
+travis_retry docker-compose run --rm php ./vendor/bin/doctrine-module orm:clear-cache:result
+travis_retry docker-compose run --rm php ./vendor/bin/doctrine-module orm:generate-proxies
+travis_retry docker-compose run --rm php ./vendor/bin/doctrine-module orm:ensure-production-settings
+travis_retry docker-compose run --rm php ./vendor/bin/doctrine-module orm:info
+travis_retry docker-compose run --rm php ./vendor/bin/doctrine-module orm:schema-tool:create
+travis_retry docker-compose run --rm php ./vendor/bin/doctrine-module orm:schema-tool:update
+travis_retry docker-compose run --rm php ./vendor/bin/doctrine-module orm:validate-schema
+travis_retry docker-compose run --rm php ./vendor/bin/doctrine-module dbal:run-sql "SELECT COUNT(a.id) FROM entity a"
+travis_retry docker-compose run --rm php ./vendor/bin/doctrine-module orm:run-dql "SELECT COUNT(a) FROM DoctrineORMModule\Travis\Entity\Entity a"
+travis_retry docker-compose run --rm php ./vendor/bin/doctrine-module orm:schema-tool:drop --dump-sql
+travis_retry docker-compose run --rm php ./vendor/bin/doctrine-module orm:schema-tool:drop --force
+travis_retry docker-compose run --rm php ./vendor/bin/doctrine-module migrations:generate
+travis_retry docker-compose run --rm php ./vendor/bin/doctrine-module migrations:diff
+travis_retry docker-compose run --rm php ./vendor/bin/doctrine-module migrations:execute 20120714005702 -n
+travis_retry docker-compose run --rm php ./vendor/bin/doctrine-module migrations:migrate
