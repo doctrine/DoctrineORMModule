d375c2d49719c5acfa4a5ee8ea90713ad6d821d6
Tom H Anderson <tom.h.anderson@gmail.com>
2020-03-08 13:55:43 -0600

    Functional testing of migrations:migrate command creates an unanswereed prompt

--- .travis/run-cli.sh
+++ .travis/run-cli.sh
@@ -16,4 +16,4 @@
 ./vendor/bin/doctrine-module migrations:generate
 ./vendor/bin/doctrine-module migrations:diff
 ./vendor/bin/doctrine-module migrations:execute 20120714005702 -n
-./vendor/bin/doctrine-module migrations:migrate
+./vendor/bin/doctrine-module migrations:migrate --no-interaction
