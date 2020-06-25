bf29ecd71ac20315acf0640fd65a72afc42540f0
Pim Jansen <pjansen@senet.nl>
2020-06-25 16:09:11 +0200

    Updated invalid namespace reference

--- config/module.config.php
+++ config/module.config.php
@@ -1,6 +1,6 @@
 <?php
 
-use Doctrine\Common\Persistence\Mapping\Driver\MappingDriverChain;
+use Doctrine\Persistence\Mapping\Driver\MappingDriverChain;
 use Doctrine\DBAL\Tools\Console;
 use Doctrine\ORM\Tools\Console\Command;
 use DoctrineModule\Form\Element;
