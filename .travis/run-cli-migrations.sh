d8d9bb39133bcb70884daabff48ae2b6c88b0a33
Grégoire Paris <postmaster@greg0ire.fr>
2020-12-07 09:15:40 +0100

    Migrate to Github Actions

--- .travis/run-cli-migrations.sh
+++
@@ -1,4 +0,0 @@
-./vendor/bin/doctrine-module migrations:generate
-./vendor/bin/doctrine-module migrations:diff
-./vendor/bin/doctrine-module migrations:execute 20120714005702 -n
-./vendor/bin/doctrine-module migrations:migrate --no-interaction
--- ci/Version20120714005702.php
+++ ci/Version20120714005702.php
@@ -17,7 +17,7 @@
  * <http://www.doctrine-project.org>.
  */
 
-namespace TravisDoctrineMigrations;
+namespace CiDoctrineMigrations;
 
 use Doctrine\DBAL\Schema\Schema,
     Doctrine\Migrations\AbstractMigration;
