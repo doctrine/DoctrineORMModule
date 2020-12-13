d9a134ceffdf5337951430ad4e0b73cf015b09d4
Tom H Anderson <tom.h.anderson@gmail.com>
2020-12-13 13:12:12 -0700

    Adjust ci config for 3.0.x

--- ci/run-cli-migrations.sh
+++ ci/run-cli-migrations.sh
@@ -1,11 +1,4 @@
 ./vendor/bin/doctrine-module migrations:generate
-./vendor/bin/doctrine-module migrations:diff -n
-./vendor/bin/doctrine-module migrations:execute --up 'CiDoctrineMigrations\Version20120714005702' -n
-./vendor/bin/doctrine-module migrations:migrate -n
-./vendor/bin/doctrine-module migrations:sync-metadata-storage
-./vendor/bin/doctrine-module migrations:list
-./vendor/bin/doctrine-module migrations:current
-./vendor/bin/doctrine-module migrations:latest
-./vendor/bin/doctrine-module migrations:up-to-date
-./vendor/bin/doctrine-module migrations:status --no-interaction
-./vendor/bin/doctrine-module migrations:version --delete 'CiDoctrineMigrations\Version20120714005702' -n
+./vendor/bin/doctrine-module migrations:diff
+./vendor/bin/doctrine-module migrations:execute 20120714005702 -n
+./vendor/bin/doctrine-module migrations:migrate --no-interaction
